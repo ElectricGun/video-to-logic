f8a750eedaafc45c846cdbff2bbdbb559ae87b32
ElectricGun <68138799+ElectricGun@users.noreply.github.com>
2023-06-04 17:51:39 +0700

    added comments and fixed stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
 let Renderer = require("v2logic/renderer")
 let Commands = require("message-block-commands/message-executor")
 
-var selfDestruct;
-
 Events.on(ClientLoadEvent, () => {
     Commands.setHeader("/", "Video Maker")
     Commands.addCommand("v2logic",
