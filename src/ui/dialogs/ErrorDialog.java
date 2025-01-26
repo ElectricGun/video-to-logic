ebfa2937a89a438561f711e26749bd2838076e5b
electricgun <-@>
2025-01-26 19:04:06 +0700

    assert correct platform upon clicking the main button

---
+++ src/ui/dialogs/ErrorDialog.java
@@ -0,0 +1,30 @@
+package ui.dialogs;
+
+import arc.Core;
+import arc.util.OS;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class ErrorDialog extends BaseDialog {
+    public ErrorDialog(String title) {
+        super(title);
+
+        String text;
+
+        if (OS.isLinux) {
+            text = "This version of Video to Logic is not supported on your device\nPlease install the[green] Linux [white]or [green]Platform [white]version in the Github repository.";
+        } else if (OS.isWindows) {
+            text = "This version of Video to Logic is not supported on your device\nPlease install the [green]Windows [white]or [green]Platform [white]version in the Github repository.";
+        } else if  (OS.isMac) {
+            text = "This version of Video to Logic is not supported on your device\nPlease install the [green]Mac [white]or [green]Platform [white]version in the Github repository.";
+        } else {
+            text = "Video to Logic is not supported on your device\nPlease use a Desktop computer, or try the [green]Platform [white]version of the mod [NOT GUARANTEED TO WORK]";
+        }
+
+        text += "\nOtherwise, [red]disable this mod.";
+
+        addCloseButton();
+        cont.add(text).pad(10).row();
+        cont.button("Github", () -> Core.app.openURI("https://github.com/ElectricGun/video-to-logic/releases")).width(200).pad(30).row();
+        show();
+    }
+}
